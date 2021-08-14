3e3e862c94e528e204465217cca8e6b84ee4d366
moe-elsadig <m.da7th@gmail.com>
2021-08-14 17:54:53 +0200

    header init

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="border border-purple-600 grid grid-flow-col grid-cols-3 justify-items-center h-[100px]">
+    <div className="sticky top-0 z-50 shadow-md grid grid-flow-col grid-cols-3 justify-items-center h-[100px] items-center">
       <div className="border border-purple-300">
         <h1>left</h1>
       </div>
@@ -10,7 +10,11 @@ function Header() {
         <h1>center</h1>
       </div>
       <div className="border border-purple-100">
-        <h1>right</h1>
+        <div className="flex space-x-4 justif">
+          <h1>right</h1>
+          <h1>right</h1>
+          <h1>right</h1>
+        </div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="min-h-screen flex flex-col">
       <Head>
         <title>Portfolio - NextJS - TW</title>
         <meta name="description" content="Welcome to me, Moe" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className="border border-red-600">Main</main>
+      <main className="border border-red-600 flex-1">Main</main>
 
       <footer className="border border-green-600">footer</footer>
     </div>
