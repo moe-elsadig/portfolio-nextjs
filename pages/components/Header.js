8297129c01100950428f763e2389ad6a3c833438
moe-elsadig <m.da7th@gmail.com>
2021-08-14 17:15:14 +0200

    install tailwindcss

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Header() {
-  return <div>Header component</div>;
+  return (
+    <div className="border border-purple-600 grid grid-flow-col grid-cols-3 justify-items-center h-[100px]">
+      <div className="border border-purple-300">
+        <h1>left</h1>
+      </div>
+      <div className="border border-purple-200">
+        <h1>center</h1>
+      </div>
+      <div className="border border-purple-100">
+        <h1>right</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
