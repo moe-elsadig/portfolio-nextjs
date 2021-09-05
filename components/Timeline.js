42ee2c08c9ca01b7ccd55f88e23bdc18171f238c
moe-elsadig <m.da7th@gmail.com>
2021-09-05 20:36:39 +0200

    styling and data for the work timeline

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,134 +1,161 @@
 import React from "react";
 import Image from "next/image";
+import { VscDebugBreakpointLog } from "react-icons/vsc";
 
 const data = [
   {
-    startDate: "Jan 2021",
-    endDate: "Jan 2021",
-    position: "Employment Positiion",
-    company: "Udacom",
-    companyLogo: "",
+    dateStart: "July 2019",
+    dateEnd: "Present",
+    position: "Founder/Developer",
+    company: "Sai eServices",
+    location: "Khartoum, SD",
+    companyLogo: "/work/sai-logo.png",
     description: [
-      "lorem ipsum dolor sit amet, consectetur adip",
-      "lorem ipsum",
+      "Founded and developed a local eCommerce platform aimed at improving and empowering entrepreneurs in a unique national economic landscape, alSa3i.coms",
     ],
     relatedProjects: [],
   },
   {
-    startDate: "Jan 2021",
-    endDate: "Jan 2021",
-    position: "Employment Positiion",
-    company: "Udacom",
-    companyLogo: "",
+    dateStart: "May 2018",
+    dateEnd: "Jul 2019",
+    position: "AUYVC/Data & Systems Admin.",
+    company: "African Union Commission",
+    location: "Addis Ababa, ET",
+    companyLogo: "/work/auc-logo.png",
     description: [
-      "lorem ipsum dolor sit amet, consectetur adip",
-      "lorem ipsum",
+      "Responsible for the Data, Systems and Integrated Geoportal of the Project Mgmt. Team of GMES & Africa",
+      "Lead the early development of the project’s platforms (Web platform, eLearning, GeoPortal, Forum)",
+      "Admin for IT related matters of the program as an ICT Expert",
     ],
     relatedProjects: [],
   },
   {
-    startDate: "Jan 2021",
-    endDate: "Jan 2021",
-    position: "Employment Positiion",
-    company: "Udacom",
-    companyLogo: "",
+    dateStart: "Oct 2017",
+    dateEnd: "Jan 2019",
+    position: "Independent Consulting",
+    company: "Udacity",
+    location: "Remote, US",
+    companyLogo: "/work/udacity-logo.png",
     description: [
-      "lorem ipsum dolor sit amet, consectetur adip",
-      "lorem ipsum",
+      "I utilized my specialized knowledge in the fields of Android Development (Java) & Self-Driving (autonomous) Cars and my strong communication skills to provide project reviews, code reviews and other student support services",
+      ,
     ],
     relatedProjects: [],
   },
   {
-    startDate: "Jan 2021",
-    endDate: "Jan 2021",
-    position: "Employment Positiion",
-    company: "Udacom",
-    companyLogo: "",
+    dateStart: "Sep 2014",
+    dateEnd: "Dec 2014",
+    position: "Electrical/Mechanical Design (Intern)",
+    company: "Alfaid Engineering",
+    location: "Amman, JO",
+    companyLogo: "/work/meco-logo.jpeg",
     description: [
-      "lorem ipsum dolor sit amet, consectetur adip",
-      "lorem ipsum",
+      "Designed full electrical schematics and assisted with mechanical schematics for residential buildings",
+      ,
     ],
     relatedProjects: [],
   },
   {
-    startDate: "Jan 2021",
-    endDate: "Jan 2021",
-    position: "Employment Positiion",
-    company: "Udacom",
-    companyLogo: "",
+    dateStart: "Sep 2012",
+    dateEnd: "Dec 2012",
+    position: "Maintenance Eng. (Intern)",
+    company: "Petronas Sudan",
+    location: "Khartoum, SD",
+    companyLogo: "/work/petronas-logo.jpg",
     description: [
-      "lorem ipsum dolor sit amet, consectetur adip",
-      "lorem ipsum",
-    ],
-    relatedProjects: [],
-  },
-  {
-    startDate: "Jan 2021",
-    endDate: "Jan 2021",
-    position: "Employment Positiion",
-    company: "Udacom",
-    companyLogo: "",
-    description: [
-      "lorem ipsum dolor sit amet, consectetur adip",
-      "lorem ipsum",
+      "Administered the full setup and initialization of a computerised maintenance management system",
+      ,
     ],
     relatedProjects: [],
   },
 ];
+
 function Timeline() {
-  let rightMarkup = () => (
+  let rightMarkup = ({
+    company,
+    companyLogo,
+    location,
+    dateStart,
+    dateEnd,
+    position,
+    description,
+  }) => (
     <div className="relative z-10">
-      <img
-        src="https://images.pexels.com/photos/6291572/pexels-photo-6291572.jpeg?auto=compress&cs=tinysrgb&h=100"
-        className="timeline-img"
-      />
+      <div className="timeline-img">
+        <Image
+          src={companyLogo}
+          alt=""
+          layout="fill"
+          objectFit="cover"
+          className="rounded-full"
+        />
+      </div>
       <div className="timeline-container">
         <div className="timeline-pointer" aria-hidden="true"></div>
         <div className="bg-white p-6 rounded-md shadow-md">
           <span className="font-bold text-indigo-400 text-sm tracking-wide">
-            Jan 2021
+            {dateStart} - {dateEnd}
           </span>
-          <h1 className="text-2xl font-bold pt-1">Project</h1>
-          <p className="pt-1">
-            Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit
-            amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip
-            Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit
-            amet, consectetur adip
-          </p>
+          <h1 className="text-xl font-bold pt-1">{position}</h1>
+          <h2 className="text-lg font-semibold text-gray-300">
+            {company} - {location}
+          </h2>
+          {description.map((item, index) => (
+            <p key={item} className="pt-1 flex flex-row items-center space-x-2">
+              <VscDebugBreakpointLog className="h-2" />
+              {item}
+            </p>
+          ))}
         </div>
       </div>
     </div>
   );
 
-  let leftMarkup = () => (
+  let leftMarkup = ({
+    company,
+    companyLogo,
+    location,
+    dateStart,
+    dateEnd,
+    position,
+    description,
+  }) => (
     <div className="relative z-10">
-      <img
-        src="https://images.pexels.com/photos/6291572/pexels-photo-6291572.jpeg?auto=compress&cs=tinysrgb&h=100"
-        className="timeline-img"
-      />
+      <div className="timeline-img">
+        <Image
+          src={companyLogo}
+          alt=""
+          layout="fill"
+          objectFit="cover"
+          className="rounded-full"
+        />
+      </div>
       <div className="timeline-container timeline-container-left">
         <div
           className="timeline-pointer timeline-pointer-left"
           aria-hidden="true"
         ></div>
         <div className="bg-white p-6 rounded-md shadow-md">
-          <span className="font-bold text-indigo-600 text-sm tracking-wide">
-            Jan 2021
+          <span className="font-bold text-indigo-400 text-sm tracking-wide">
+            {dateStart} - {dateEnd}
           </span>
-          <h1 className="text-2xl font-bold pt-1">Project</h1>
-          <p className="pt-1">
-            Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit
-            amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip
-            Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit
-            amet, consectetur adip
-          </p>
+          <h1 className="text-xl font-bold pt-1">{position}</h1>
+          <h2 className="text-lg font-semibold text-gray-300">
+            {company} - {location}
+          </h2>
+          {description.map((item, index) => (
+            <p key={item} className="pt-1 flex flex-row items-center space-x-2">
+              <VscDebugBreakpointLog className="h-2" />
+              {item}
+            </p>
+          ))}
         </div>
       </div>
     </div>
   );
   return (
-    <div className="container mx-auto p-6 relative flex flex-col space-y-8 bg-white">
-      <div className="absolute w-2 h-full shadow-md inset-0 left-17 md:mx-auto md:right-0 md:left-0 bg-gradient-to-b from-red-400 via-blue-400 to-purple-400"></div>
+    <div className="container mx-auto p-6 my-6 relative flex flex-col space-y-8 bg-white">
+      <div className="absolute w-2 max-h-full shadow-md inset-0 left-17 md:mx-auto md:right-0 md:left-0 bg-gradient-to-b from-red-400 via-blue-400 to-purple-400 rounded-xl"></div>
       {/* container */}
 
       {data.map((employment, index) => {
