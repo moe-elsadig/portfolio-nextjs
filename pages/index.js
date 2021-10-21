981794b6357812973cd2283ebafa8ca810679436
moe-elsadig <m.da7th@gmail.com>
2021-10-21 09:35:26 +0200

    test header theme

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,16 @@ export default function Home({
           content="Welcome, I'm Moe, and this is my portfolio"
         />
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="theme-color"
+          content="#EF4444"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#374151"
+          media="(prefers-color-scheme: dark)"
+        />
       </Head>
       <Header toggleTheme={toggleTheme} />
       <ProfileCard />
