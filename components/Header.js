3b65193901b559c0da0356cc4e81e3c3d2d33f75
moe-elsadig <m.da7th@gmail.com>
2022-11-15 22:23:11 +0300

    updated work data

--- components/Header.js
+++ components/Header.js
@@ -6,222 +6,235 @@ import Headroom from "react-headroom";
 import { CgDarkMode } from "react-icons/cg";
 
 function Header({}) {
-  const [menu, setMenu] = useState(false);
-  const [showBar, setShowBar] = useState(true);
-  const [theme, setTheme] = useState(false);
+    const [menu, setMenu] = useState(false);
+    const [showBar, setShowBar] = useState(true);
+    const [theme, setTheme] = useState(false);
 
-  useEffect(() => {
-    let localTheme = localStorage.getItem("theme");
-    if (localTheme && localTheme === "true") {
-      setTheme(true);
-      document.body.classList.add("dark");
-    }
-  }, []);
+    useEffect(() => {
+        let localTheme = localStorage.getItem("theme");
+        if (localTheme && localTheme === "true") {
+            setTheme(true);
+            document.body.classList.add("dark");
+        }
+    }, []);
 
-  const changeTheme = () => {
-    localStorage.setItem("theme", !theme);
-    setTheme(!theme);
-    if (!theme) {
-      document.body.classList.add("dark");
-    } else {
-      document.body.classList.remove("dark");
-    }
-  };
+    const changeTheme = () => {
+        localStorage.setItem("theme", !theme);
+        setTheme(!theme);
+        if (!theme) {
+            document.body.classList.add("dark");
+        } else {
+            document.body.classList.remove("dark");
+        }
+    };
 
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
 
-  const scrollToHome = () => {
-    const el = document.getElementById("profile-card");
-    el.scrollIntoView({
-      behavior: "smooth",
-      block: "end",
-      inline: "nearest",
-    });
-    setMenu(false);
-  };
+    const scrollToHome = () => {
+        const el = document.getElementById("profile-card");
+        el.scrollIntoView({
+            behavior: "smooth",
+            block: "end",
+            inline: "nearest",
+        });
+        setMenu(false);
+    };
 
-  const scrollToWork = () => {
-    const el = document.getElementById("work-section");
-    el.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
-    setMenu(false);
-  };
+    const scrollToWork = () => {
+        const el = document.getElementById("work-section");
+        el.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+        setMenu(false);
+    };
 
-  const scrollToProjects = () => {
-    const el = document.getElementById("projects-section");
-    el.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
-    setMenu(false);
-  };
+    const scrollToProjects = () => {
+        const el = document.getElementById("projects-section");
+        el.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+        setMenu(false);
+    };
 
-  const scrollToEducation = () => {
-    const el = document.getElementById("education-section");
-    el.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
-    setMenu(false);
-  };
+    const scrollToEducation = () => {
+        const el = document.getElementById("education-section");
+        el.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+        setMenu(false);
+    };
 
-  const scrollToContact = () => {
-    const el = document.getElementById("contact-section");
-    el.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
-    setMenu(false);
-  };
+    const scrollToContact = () => {
+        const el = document.getElementById("contact-section");
+        el.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+        setMenu(false);
+    };
 
-  let prevPos = 0;
+    let prevPos = 0;
 
-  const handleScroll = () => {
-    var currentScrollPos = window.pageYOffset;
+    const handleScroll = () => {
+        var currentScrollPos = window.pageYOffset;
 
-    // console.log(prevPos, currentScrollPos);
-    if (prevPos > currentScrollPos) {
-      // hide
-      prevPos = currentScrollPos;
-      setShowBar(false);
-      // console.log("bar", showBar);
-    } else {
-      // show
-      prevPos = currentScrollPos;
-      setShowBar(true);
-      // console.log("bar", showBar);
-    }
-  };
+        // console.log(prevPos, currentScrollPos);
+        if (prevPos > currentScrollPos) {
+            // hide
+            prevPos = currentScrollPos;
+            setShowBar(false);
+            // console.log("bar", showBar);
+        } else {
+            // show
+            prevPos = currentScrollPos;
+            setShowBar(true);
+            // console.log("bar", showBar);
+        }
+    };
 
-  useEffect(() => {
-    const handleScrollThrottled = throttle(handleScroll, 200);
-    window.addEventListener("scroll", handleScrollThrottled);
-    return () => window.removeEventListener("scroll", handleScrollThrottled);
-  }, []);
+    useEffect(() => {
+        const handleScrollThrottled = throttle(handleScroll, 200);
+        window.addEventListener("scroll", handleScrollThrottled);
+        return () =>
+            window.removeEventListener("scroll", handleScrollThrottled);
+    }, []);
 
-  let buttonsMarkup = (
-    <>
-      <button className={`${buttonStyle}`} onClick={() => scrollToHome()}>
-        Home
-      </button>
-      <button className={`${buttonStyle}`} onClick={() => scrollToProjects()}>
-        Projects
-      </button>
-      <button className={`${buttonStyle}`} onClick={() => scrollToWork()}>
-        Work
-      </button>
-      <button className={`${buttonStyle}`} onClick={() => scrollToEducation()}>
-        Education
-      </button>
-      <a
-        className={`${buttonStyle}`}
-        href="/Moe Resume - General.pdf"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Resume
-      </a>
-      <button className={`${buttonStyle}`} onClick={() => scrollToContact()}>
-        Contact
-      </button>
-      <CgDarkMode
-        className="text-2xl cursor-pointer text-black dark:text-white flex items-center"
-        onClick={() => {
-          setTheme(!theme);
-          changeTheme();
-        }}
-      />
-    </>
-  );
-  return (
-    <Headroom className="z-50">
-      <div className="sticky top-0 z-50 shadow-md flex flex-row justify-between justify-items-center h-[60px] items-center backdrop-blur-lg bg-white dark:bg-[#101010]">
-        <div className="pl-5 flex flex-row justify-between">
-          <h1
-            className="text-2xl cursor-pointer text-black dark:text-white flex items-center"
-            onClick={() => scrollToTop()}
-          >
-            Moe.
-          </h1>
-        </div>
-        <div className="hidden md:inline-flex md:max-w-[100px]">
-          <div className="inline-flex dark:hidden">
-            <Image
-              priority
-              alt=""
-              src="/logo_small_black.svg"
-              width="60"
-              height="60"
-              // layout="fill"
-              objectFit="contain"
-              className="text-2xl cursor-pointer"
-              onClick={() => scrollToTop()}
-            />
-          </div>
-
-          <div className="hidden dark:inline-flex">
-            <Image
-              priority
-              alt=""
-              src="/logo_small_white.svg"
-              width="60"
-              height="60"
-              // layout="fill"
-              objectFit="contain"
-              className="text-2xl cursor-pointer"
-              onClick={() => scrollToTop()}
-            />
-          </div>
-        </div>
-
-        <CgDarkMode
-          className=" md:hidden text-2xl cursor-pointer text-black dark:text-white flex items-center"
-          onClick={() => {
-            setTheme(!theme);
-            changeTheme();
-          }}
-        />
-        <div className="pr-5">
-          <div className="hidden space-x-4 justify-center md:inline-flex flex-row items-center">
-            {buttonsMarkup}
-          </div>
-          <button
-            className="md:hidden text-black dark:text-white"
-            onClick={() => setMenu(!menu)}
-          >
-            Menu
-          </button>
-          <div
-            style={{
-              visibility: menu ? "visible" : "hidden",
-              scale: menu ? 1 : 0.5,
-            }}
-            className="absolute z-51 top-0 left-0 h-screen w-screen bg-gray-400 dark:bg-gray-500 opacity-50"
-            onClick={() => setMenu(false)}
-          />
-          <div
-            style={{
-              visibility: menu ? "visible" : "hidden",
-            }}
-            className="md:hidden flex flex-col space-y-4 justify-start z-52 fixed top-0 right-0 bg-gradient-to-r from-transparent via-pink-400 dark:via-pink-500 to-indigo-400 dark:to-indigo-500 p-10 text-white dark:text-black text-lg transition transform ease-in-out duration-200 min-h-screen items-end"
-          >
-            <button className={`${buttonStyle}`} onClick={() => setMenu()}>
-              <XIcon className={`h-10 mx-auto animate-pulse`} />
+    let buttonsMarkup = (
+        <>
+            <button className={`${buttonStyle}`} onClick={() => scrollToHome()}>
+                Home
             </button>
-            {buttonsMarkup}
-          </div>
-        </div>
-      </div>
-    </Headroom>
-  );
+            <button className={`${buttonStyle}`} onClick={() => scrollToWork()}>
+                Work
+            </button>
+            <button
+                className={`${buttonStyle}`}
+                onClick={() => scrollToEducation()}
+            >
+                Education
+            </button>
+            <button
+                className={`${buttonStyle}`}
+                onClick={() => scrollToProjects()}
+            >
+                Projects
+            </button>
+            <a
+                className={`${buttonStyle}`}
+                href="/Moe Resume - General.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Resume
+            </a>
+            <button
+                className={`${buttonStyle}`}
+                onClick={() => scrollToContact()}
+            >
+                Contact
+            </button>
+            <CgDarkMode
+                className="text-2xl cursor-pointer text-black dark:text-white flex items-center"
+                onClick={() => {
+                    setTheme(!theme);
+                    changeTheme();
+                }}
+            />
+        </>
+    );
+    return (
+        <Headroom className="z-50">
+            <div className="sticky top-0 z-50 shadow-md flex flex-row justify-between justify-items-center h-[60px] items-center backdrop-blur-lg bg-white dark:bg-[#101010]">
+                <div className="pl-5 flex flex-row justify-between">
+                    <h1
+                        className="text-2xl cursor-pointer text-black dark:text-white flex items-center"
+                        onClick={() => scrollToTop()}
+                    >
+                        Moe.
+                    </h1>
+                </div>
+                <div className="hidden md:inline-flex md:max-w-[100px]">
+                    <div className="inline-flex dark:hidden">
+                        <Image
+                            priority
+                            alt=""
+                            src="/logo_small_black.svg"
+                            width="60"
+                            height="60"
+                            // layout="fill"
+                            objectFit="contain"
+                            className="text-2xl cursor-pointer"
+                            onClick={() => scrollToTop()}
+                        />
+                    </div>
+
+                    <div className="hidden dark:inline-flex">
+                        <Image
+                            priority
+                            alt=""
+                            src="/logo_small_white.svg"
+                            width="60"
+                            height="60"
+                            // layout="fill"
+                            objectFit="contain"
+                            className="text-2xl cursor-pointer"
+                            onClick={() => scrollToTop()}
+                        />
+                    </div>
+                </div>
+
+                <CgDarkMode
+                    className=" md:hidden text-2xl cursor-pointer text-black dark:text-white flex items-center"
+                    onClick={() => {
+                        setTheme(!theme);
+                        changeTheme();
+                    }}
+                />
+                <div className="pr-5">
+                    <div className="hidden space-x-4 justify-center md:inline-flex flex-row items-center">
+                        {buttonsMarkup}
+                    </div>
+                    <button
+                        className="md:hidden text-black dark:text-white"
+                        onClick={() => setMenu(!menu)}
+                    >
+                        Menu
+                    </button>
+                    <div
+                        style={{
+                            visibility: menu ? "visible" : "hidden",
+                            scale: menu ? 1 : 0.5,
+                        }}
+                        className="absolute z-51 top-0 left-0 h-screen w-screen bg-gray-400 dark:bg-gray-500 opacity-50"
+                        onClick={() => setMenu(false)}
+                    />
+                    <div
+                        style={{
+                            visibility: menu ? "visible" : "hidden",
+                        }}
+                        className="md:hidden flex flex-col space-y-4 justify-start z-52 fixed top-0 right-0 bg-gradient-to-r from-transparent via-pink-400 dark:via-pink-500 to-indigo-400 dark:to-indigo-500 p-10 text-white dark:text-black text-lg transition transform ease-in-out duration-200 min-h-screen items-end"
+                    >
+                        <button
+                            className={`${buttonStyle}`}
+                            onClick={() => setMenu()}
+                        >
+                            <XIcon className={`h-10 mx-auto animate-pulse`} />
+                        </button>
+                        {buttonsMarkup}
+                    </div>
+                </div>
+            </div>
+        </Headroom>
+    );
 }
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -9,143 +9,143 @@ import { MdExpandLess, MdExpandMore } from "react-icons/md";
 import ContactCard from "../components/ContactCard";
 
 export default function Home({
-  projectData,
-  featuredProjectData,
-  workData,
-  educationData,
+    projectData,
+    featuredProjectData,
+    workData,
+    educationData,
 }) {
-  const [accentColor, setAccentColor] = useState("red");
-  const [showMore, setShowMore] = useState(false);
+    const [accentColor, setAccentColor] = useState("red");
+    const [showMore, setShowMore] = useState(false);
 
-  return (
-    <div
-      className={`min-h-screen flex flex-col font-mono relative text-black dark:text-white bg-gray-100 dark:bg-gray-700`}
-    >
-      <Head>
-        <title>Moe&#39;s Portfolio</title>
-        <meta
-          name="description"
-          content="Welcome, I'm Moe, and this is my portfolio"
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="theme-color"
-          content="#EF4444"
-          media="(prefers-color-scheme: light)"
-        />
-        <meta
-          name="theme-color"
-          content="#374151"
-          media="(prefers-color-scheme: dark)"
-        />
-      </Head>
-      <Header />
-      {/* <div className="h-[500px] w-full flex items-center justify-center">
+    return (
+        <div
+            className={`min-h-screen flex flex-col font-mono relative text-black dark:text-white bg-gray-100 dark:bg-gray-700`}
+        >
+            <Head>
+                <title>Moe&#39;s Portfolio</title>
+                <meta
+                    name="description"
+                    content="Welcome, I'm Moe, and this is my portfolio"
+                />
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="theme-color"
+                    content="#EF4444"
+                    media="(prefers-color-scheme: light)"
+                />
+                <meta
+                    name="theme-color"
+                    content="#374151"
+                    media="(prefers-color-scheme: dark)"
+                />
+            </Head>
+            <Header />
+            {/* <div className="h-[500px] w-full flex items-center justify-center">
         <TravelGlobe dimensions={1000} />
       </div> */}
-      <ProfileCard />
-      <SectionHeader title="projects" />
-      <main className="relative bg-gray-100 dark:bg-gray-700 antialiased">
-        {projectData?.slice(0, 3).map((project, index) => (
-          <ProjectCard
-            priorityImages={true}
-            key={project.title}
-            project={project}
-            index={index}
-            accentColor={accentColor}
-          />
-        ))}
+            <ProfileCard />
+            <SectionHeader title="work" />
+            <Timeline timelineType="work" cardData={workData} />
+            <SectionHeader title="education" />
+            <Timeline timelineType="education" cardData={educationData} />
+            <SectionHeader title="projects" />
+            <section className="relative bg-gray-100 dark:bg-gray-700 antialiased">
+                {projectData?.slice(0, 3).map((project, index) => (
+                    <ProjectCard
+                        priorityImages={true}
+                        key={project.title}
+                        project={project}
+                        index={index}
+                        accentColor={accentColor}
+                    />
+                ))}
 
-        {showMore &&
-          projectData
-            ?.slice(3)
-            .map((project, index) => (
-              <ProjectCard
-                key={project.title}
-                project={project}
-                index={index + 3}
-                accentColor={accentColor}
-              />
-            ))}
+                {showMore &&
+                    projectData
+                        ?.slice(3)
+                        .map((project, index) => (
+                            <ProjectCard
+                                key={project.title}
+                                project={project}
+                                index={index + 3}
+                                accentColor={accentColor}
+                            />
+                        ))}
 
-        <div className="max-w-screen-2xl mx-auto pb-10 bg-white dark:bg-[#101010] flex flex-col">
-          {/* <SectionHeader title="Projects" /> */}
-          {projectData.length > 1 && (
-            <button
-              onClick={() => {
-                setShowMore(!showMore);
-              }}
-              className="flex flex-grow items-center self-start md:self-center py-2 z-10 shadow-md px-4 rounded-md backdrop-blur-lg text-black dark:text-white"
-            >
-              Show {showMore ? "less" : "more"}{" "}
-              {showMore ? (
-                <MdExpandLess className=" text-red-400" />
-              ) : (
-                <MdExpandMore className=" text-red-400" />
-              )}
-            </button>
-          )}
+                <div className="max-w-screen-2xl mx-auto pb-10 bg-white dark:bg-[#101010] flex flex-col">
+                    {/* <SectionHeader title="Projects" /> */}
+                    {projectData.length > 1 && (
+                        <button
+                            onClick={() => {
+                                setShowMore(!showMore);
+                            }}
+                            className="flex flex-grow items-center self-start md:self-center py-2 z-10 shadow-md px-4 rounded-md backdrop-blur-lg text-black dark:text-white"
+                        >
+                            Show {showMore ? "less" : "more"}{" "}
+                            {showMore ? (
+                                <MdExpandLess className=" text-red-400" />
+                            ) : (
+                                <MdExpandMore className=" text-red-400" />
+                            )}
+                        </button>
+                    )}
+                </div>
+            </section>
+            <SectionHeader title="contact" />
+            <ContactCard />
+            <footer className="border-t bg-gray-100 dark:bg-gray-700">
+                <p className="max-w-screen-2xl text-sm text-gray-400 dark:text-gray-500 px-10 pt-10 mx-auto bg-white dark:bg-[#101010]">
+                    Designed & Developed by{" "}
+                    <span className="text-red-400 dark:text-red-500">M</span>
+                    <span className="text-red-400 dark:text-red-500">o</span>
+                    <span className="text-red-400 dark:text-red-500">e</span>
+                    <span className="text-red-400 dark:text-red-500">.</span>
+                </p>
+            </footer>
         </div>
-      </main>
-      <SectionHeader title="education" />
-      <Timeline timelineType="education" cardData={educationData} />
-      <SectionHeader title="work" />
-      <Timeline timelineType="work" cardData={workData} />
-      <SectionHeader title="contact" />
-      <ContactCard />
-      <footer className="border-t bg-gray-100 dark:bg-gray-700">
-        <p className="max-w-screen-2xl text-sm text-gray-400 dark:text-gray-500 px-10 pt-10 mx-auto bg-white dark:bg-[#101010]">
-          Designed & Developed by{" "}
-          <span className="text-red-400 dark:text-red-500">M</span>
-          <span className="text-red-400 dark:text-red-500">o</span>
-          <span className="text-red-400 dark:text-red-500">e</span>
-          <span className="text-red-400 dark:text-red-500">.</span>
-        </p>
-      </footer>
-    </div>
-  );
+    );
 }
 
 export async function getServerSideProps() {
-  const NODE_ENV = process.env.NODE_ENV;
-  // production url
-  let projectDataURL = "https://portfolio-nextjs-psi.vercel.app";
+    const NODE_ENV = process.env.NODE_ENV;
+    // production url
+    let projectDataURL = "https://portfolio-nextjs-psi.vercel.app";
 
-  if (NODE_ENV === "development") {
-    projectDataURL = "http://localhost:3000";
-  }
+    if (NODE_ENV === "development") {
+        projectDataURL = "http://localhost:3000";
+    }
 
-  const [projectData, featuredProjectData] = await fetch(
-    projectDataURL + "/api/projectData"
-  )
-    .then((res) => res.json())
-    .catch((err) => {
-      console.log("unable to fetch project data");
-      return null;
-    });
+    const [projectData, featuredProjectData] = await fetch(
+        projectDataURL + "/api/projectData"
+    )
+        .then((res) => res.json())
+        .catch((err) => {
+            console.log("unable to fetch project data");
+            return null;
+        });
 
-  const workData = await fetch(projectDataURL + "/api/workData")
-    .then((res) => res.json())
-    .catch((err) => {
-      console.log("unable to fetch project data");
-      return null;
-    });
+    const workData = await fetch(projectDataURL + "/api/workData")
+        .then((res) => res.json())
+        .catch((err) => {
+            console.log("unable to fetch project data");
+            return null;
+        });
 
-  const educationData = await fetch(projectDataURL + "/api/educationData")
-    .then((res) => res.json())
-    .catch((err) => {
-      console.log("unable to fetch project data");
-      return null;
-    });
+    const educationData = await fetch(projectDataURL + "/api/educationData")
+        .then((res) => res.json())
+        .catch((err) => {
+            console.log("unable to fetch project data");
+            return null;
+        });
 
-  console.log(
-    projectData.length,
-    featuredProjectData.length,
-    workData.length,
-    educationData.length
-  );
+    console.log(
+        projectData.length,
+        featuredProjectData.length,
+        workData.length,
+        educationData.length
+    );
 
-  return {
-    props: { projectData, featuredProjectData, workData, educationData },
-  };
+    return {
+        props: { projectData, featuredProjectData, workData, educationData },
+    };
 }
